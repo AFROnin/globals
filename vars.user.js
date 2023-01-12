8f6f7fdf9fce54c137c61b8971f845372a8758b0
Kavitation <86653730+Kavitation@users.noreply.github.com>
2023-01-12 15:23:08 +0100

    Update vars.user.js

--- vars.user.js
+++ vars.user.js
@@ -1,4 +1,4 @@
-const test = "this is a test"
+const test = "this is a productive system and no one plans to ever change it"
 
 // https://raw.githubusercontent.com/:owner/:repo/master/:path
 // nice test!
